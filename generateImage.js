fa15504d1ffcb0500453f5436e2114ced5c13d06
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-04-18 22:39:55 +0530

    Files updated

--- generateImage.js
+++ generateImage.js
@@ -1,11 +1,10 @@
 const Canvas = require("canvas");
 const Discord = require("discord.js");
-const background =
-  "https://drive.google.com/file/d/1_VWUKkH_vlOGZyQhmpeDM8Gn5zIs6AH4/view?usp=sharing";
+const background = "https://www.linkpicture.com/q/SLIITFOSS-banner.png";
 
 const dim = {
-  height: 675,
-  width: 1200,
+  height: 720,
+  width: 1280,
   margin: 50,
 };
 
@@ -17,32 +16,36 @@ const av = {
 
 const generateImage = async (member) => {
   let username = member.user.username;
-  let discriminator = member.user.discriminator;
+  let discrim = member.user.discriminator;
   let avatarURL = member.user.displayAvatarURL({
     format: "png",
     dynamic: false,
     size: av.size,
   });
+
   const canvas = Canvas.createCanvas(dim.width, dim.height);
   const ctx = canvas.getContext("2d");
 
-  const backgroundImage = await Canvas.loadImage(background);
-  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-  //draw black rectangle
+  // draw in the background
+  const backimg = await Canvas.loadImage(background);
+  ctx.drawImage(backimg, 0, 0);
 
-  ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+  // draw black tinted box
+  ctx.fillStyle = "rgba(0,0,0,0.8)";
   ctx.fillRect(
     dim.margin,
     dim.margin,
-    dim.width - dim.margin * 2,
-    dim.height - dim.margin * 2
+    dim.width - 2 * dim.margin,
+    dim.height - 2 * dim.margin
   );
-  const avatar = await Canvas.loadImage(avatarURL);
+
+  const avimg = await Canvas.loadImage(avatarURL);
   ctx.save();
+
   ctx.beginPath();
   ctx.arc(
-    (av.x + av.y) / 2,
-    (av.y + av.size) / 2,
+    av.x + av.size / 2,
+    av.y + av.size / 2,
     av.size / 2,
     0,
     Math.PI * 2,
@@ -50,18 +53,37 @@ const generateImage = async (member) => {
   );
   ctx.closePath();
   ctx.clip();
-  ctx.drawImage(avatar, av.x, av.y);
+
+  ctx.drawImage(avimg, av.x, av.y);
   ctx.restore();
 
-  //write in text
+  // write in text
   ctx.fillStyle = "white";
   ctx.textAlign = "center";
 
-  ctx.font = "bold 50px Roboto";
+  // draw in Welcome
+  ctx.font = "50px Roboto";
+  ctx.fillText("Welcome", dim.width / 2, dim.margin + 70);
+
+  // draw in the username
+  ctx.font = "60px Roboto";
+  ctx.fillText(
+    username + discrim,
+    dim.width / 2,
+    dim.height - dim.margin - 125
+  );
+
+  // draw in to the server
+  ctx.font = "40px Roboto";
+  ctx.fillText(
+    "to the SLIIT FOSS Community server",
+    dim.width / 2,
+    dim.height - dim.margin - 50
+  );
 
   const attachment = new Discord.MessageAttachment(
     canvas.toBuffer(),
-    "welcome-image.png"
+    "welcome.png"
   );
   return attachment;
 };
--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const Discord = require("discord.js");
-const generateImage = require("./generateImage");
 require("dotenv").config();
-const TOKEN = process.env.DISCORD_TOKEN;
+
+const generateImage = require("./generateImage");
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("messageCreate", (message) => {
-  if (message.content === "Hi") {
-    message.reply("Welcome to SLIIT FOSS Community Discord Server");
-  } else if (message.content === "hi") {
-    message.reply("Welcome to SLIIT FOSS Community Discord Server");
-  } else if (message.content === "miyuru") {
-    message.reply("Miyuru Balliyek");
-  } else if (message.content === "akalanka") {
-    message.reply("He likes cats");
+  if (message.content == "hi") {
+    message.reply("Welcome to the server!");
+  } else if (message.content == "miyuru") {
+    message.reply("Miyuru playboy");
+  } else if (message.content == "akalanka") {
+    message.reply("Podi eka");
+  } else if (message.content == "nisal") {
+    message.reply("Good Boy");
+  } else if (message.content == "sithum") {
+    message.reply("Go deyyo");
   }
 });
 
@@ -28,9 +30,9 @@ const welcomeChannelId = process.env.WELCOME_CHANNEL_ID;
 client.on("guildMemberAdd", async (member) => {
   const img = await generateImage(member);
   member.guild.channels.cache.get(welcomeChannelId).send({
-    content: `${member} Welcome to SLIIT FOSS Community Discord Server `,
+    content: `${member} Welcome to the  SLIIT FOSS Community server!`,
     files: [img],
   });
 });
 
-client.login(TOKEN);
+client.login(process.env.DISCORD_TOKEN);
